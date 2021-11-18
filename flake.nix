ef7ec537d5c89d1d8c6c4b8b5db9037f5857b8f2
Yureka <yuka@yuka.dev>
2021-11-18 18:24:03 +0100

    simplify flake setup

--- flake.nix
+++ flake.nix
@@ -2,40 +2,22 @@
   description = "ntp-proxy";
 
   outputs = { self, nixpkgs }: let
-    version = self.shortRev or (toString self.lastModifiedDate);
     overlay = final: prev: {
       ntp-proxy = final.callPackage (
         { rustPlatform }: rustPlatform.buildRustPackage {
           pname = "ntp-proxy";
-          inherit version;
+          version = self.shortRev or "dirty-${toString self.lastModifiedDate}";
           src = self;
           cargoLock.lockFile = ./Cargo.lock;
         }
       ) {};
-
-      ntp-proxy-pkg = final.callPackage (
-        { ntp-proxy, zstd }: pkgs.runCommand "ntp-proxy-pkg" {
-          nativeBuildInputs = [ zstd ];
-        } ''
-          mkdir -p usr/bin $out
-          cp ${ntp-proxy}/bin/ntp_proxy usr/bin
-          tar --zstd -cf $out/ntp-proxy-x86_64-${ntp-proxy.version}.pkg usr
-        ''
-      ) {};
-    };
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-      crossSystem = {
-        isStatic = true;
-        config = "x86_64-unknown-linux-musl";
-      };
-      overlays = [ overlay ];
     };
   in {
     inherit overlay;
-    packages.x86_64-linux = {
-      inherit (pkgs) ntp-proxy ntp-proxy-pkg;
+    packages.x86_64-linux = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [ overlay ];
     };
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.ntp-proxy-pkg;
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.ntp-proxy;
   };
 }
